8d8dd2a92bae83a68c56ea24f48d7a0097ab5d70
Tim Wendel <wendel@milongroup.com>
2023-01-25 16:30:30 +0100

    Fixed typo.

--- proto/generate.go
+++ proto/generate.go
@@ -5,6 +5,4 @@ package proto
 // 		--go_out= where to put the go files. Two points so that the compiler does not create a subfolder
 // 		path to the file.
 //go:generate protoc -I=. --go_out=.. ./addressbook.proto
-
-// Does not work....
-//go:generate protoc -I=. --go_out=.. --go-grpc-out=.. ./greeter.proto
+//go:generate protoc -I=. --go_out=.. --go-grpc_out=.. ./greeter.proto
