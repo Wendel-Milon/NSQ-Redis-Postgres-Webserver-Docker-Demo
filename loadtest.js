febe2040f826e2293a49f35715e9dfff90bc6acf
Tim Wendel <wendel@milongroup.com>
2023-01-30 12:15:49 +0100

    Added Grafana and Loki for observability.

--- loadtest.js
+++ loadtest.js
@@ -3,21 +3,21 @@ import { check, sleep } from 'k6';
 
 export const options = {
   stages: [
-    { duration: '25s', target: 1000 },
+    { duration: '60s', target: 5000 },
     // { duration: '1m30s', target: 10 },
     // { duration: '20s', target: 0 },
   ],
 };
 
 // Load test the protected route.
-// export default function () {
-//   const res = http.get('http://localhost:8080/protected', {
-//     cookies: {
-//         csrftoken: "78b5a6c4-80cf-4146-b363-2c7068644bc3",
-//     }
-//   });
-//   check(res, { 'status was 200': (r) => r.status == 200 });
-// }
+export default function () {
+  const res = http.get('http://localhost:8080', {});
+  //   cookies: {
+  //       csrftoken: "78b5a6c4-80cf-4146-b363-2c7068644bc3",
+  //   }
+  // });
+  check(res, { 'status was 200': (r) => r.status == 200 });
+}
 
 
 // export default function () {
@@ -32,11 +32,11 @@ export const options = {
 //   }
 
 
-  export default function () {
-    let res = http.post('http://localhost:8080/protected',{
-          cookies: {
-              csrftoken: "6ae5a2c9-2de6-4b97-b6e7-7bb900ad05df",
-          }
-        });
-    check(res, { 'status was 200': (r) => r.status == 200});
-  }
+  // export default function () {
+  //   let res = http.post('http://localhost:8080/protected',{
+  //         cookies: {
+  //             csrftoken: "6ae5a2c9-2de6-4b97-b6e7-7bb900ad05df",
+  //         }
+  //       });
+  //   check(res, { 'status was 200': (r) => r.status == 200});
+  // }
